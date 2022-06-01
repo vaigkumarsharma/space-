b604e8462e3c9bfe8fe1349a5d32dac0045405f2
vaigkumarsharma <106692380+vaigkumarsharma@users.noreply.github.com>
2022-06-01 22:09:04 +0530

    Add files via upload

---
+++ prob1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int days;
+    float year, month,daysr;
+    printf("enter number of days ");
+    scanf("%d",&days);
+    year=days/360;
+    month=days%360/30;
+    daysr=days%360-month*30;
+    printf("no of year: %f\n no of month: %f\n no of days: %f",year ,month,daysr);
+    return 0;
+    // ye wala aacha hai 
+    // 10 bajne wale hai 
+    
+}
